bd6b8595748bfc82303c6532802046ad2a272052
Jisoo Yoo <youzysu@gmail.com>
2023-07-11 02:00:49 +0900

    fix #1: 불필요한 주석 삭제 및 eslintrc 파일 확장자명 수정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -11,8 +11,6 @@ export function App() {
   return (
     <div>
       <Button variant={"blue"} text={"Button"} onClick={testFun}>
-        {/* <img css={{ height: "16px", width: "16px" }} src={icon} /> */}
-        {/* <ReactLogo fill={colors.textWhiteDefault} /> */}
         <ClosedSVG size={24} rgb={colors.textWhiteDefault} />
       </Button>
     </div>

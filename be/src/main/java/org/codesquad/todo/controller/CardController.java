5f6dcf6de576b037d7a33f691646e171d142b59c
DOEKYONG <apochmn@naver.com>
2023-07-11 15:39:44 +0900

    feat #10: CardController Delete 매핑 작성

--- be/src/main/java/org/codesquad/todo/controller/CardController.java
+++ be/src/main/java/org/codesquad/todo/controller/CardController.java
@@ -1,6 +1,9 @@
 package org.codesquad.todo.controller;
 
 import org.codesquad.todo.domain.card.CardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,12 @@ public class CardController {
 
 	public CardController(CardService cardService) {
 		this.cardService = cardService;
+	}
+
+	@DeleteMapping("/cards/{id}")
+	public ResponseEntity<Void> deleteCard(@PathVariable Long id) {
+		cardService.deleteCardById(id);
+		return ResponseEntity.ok().build();
 	}
 
 }

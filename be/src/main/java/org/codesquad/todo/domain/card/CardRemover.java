1e3f2185a8d5305e400ff81f7821d0f3b9a91399
DOEKYONG <apochmn@naver.com>
2023-07-11 15:40:53 +0900

    feat #10: CardRemover remove 메서드 작성

--- be/src/main/java/org/codesquad/todo/domain/card/CardRemover.java
+++ be/src/main/java/org/codesquad/todo/domain/card/CardRemover.java
@@ -1,4 +1,17 @@
 package org.codesquad.todo.domain.card;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CardRemover {
+
+	private CardRepository cardRepository;
+
+	public CardRemover(CardRepository cardRepository) {
+		this.cardRepository = cardRepository;
+	}
+
+	public void remove(Long id) {
+		cardRepository.remove(id);
+	}
 }

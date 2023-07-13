67e5ed05b551dea6c23f16e34ca4961d4e3ab5e3
jay <chlwodnjs34@gmail.com>
2023-07-13 16:22:29 +0900

    feat #35 : 삭제 확인 모달창 구현
    - Button 컴포넌트 내부 VARIANTS에 red background 수정

---
+++ fe/src/components/base/RemoveModal.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from "react";
+import { Button } from "./Button";
+
+interface CancelModalProps {
+  text: string;
+  isOpen: boolean;
+  removeHandler: () => void;
+  closeHandler: () => void;
+}
+
+export const RemoveModal = ({
+  text,
+  isOpen,
+  removeHandler,
+  closeHandler,
+}: CancelModalProps) => {
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    isOpen && modalRef.current?.showModal();
+  }, [isOpen]);
+
+  const handleClickClose = () => {
+    closeHandler();
+    modalRef.current?.close();
+  };
+
+  return (
+    <dialog
+      css={{
+        width: "320px",
+        height: "126px",
+        padding: "24px",
+        boxSizing: "border-box",
+        "&::backdrop": {
+          background: "rgba(0, 0, 0, 0.3)",
+        },
+      }}
+      ref={modalRef}
+      onClose={handleClickClose}
+    >
+      <div
+        css={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>{text}</div>
+        <div css={{ display: "flex", justifyContent: "space-between" }}>
+          <Button variant="gray" onClick={handleClickClose} text="취소" />
+          <Button variant="red" onClick={removeHandler} text="삭제" />
+        </div>
+      </div>
+    </dialog>
+  );
+};

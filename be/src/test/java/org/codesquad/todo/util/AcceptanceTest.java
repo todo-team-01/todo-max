ba027d18b01d1a385177e319782bcb6d3d4fed62
Mandu <rjswmtk@gmail.com>
2023-07-20 15:50:27 +0900

    fix #81: 인수 테스트 TEST DB 설정 추가 (#82)

--- be/src/test/java/org/codesquad/todo/util/AcceptanceTest.java
+++ be/src/test/java/org/codesquad/todo/util/AcceptanceTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public abstract class AcceptanceTest {
 
 	@LocalServerPort
